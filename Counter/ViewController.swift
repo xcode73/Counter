2e2f163d9922c9a016785ef807aa02b249af1e08
xcode73 <nikolai.dev@icloud.com>
2024-04-10 15:48:03 +0400

    feat: Extended task, add substraction button and history textView

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,11 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet
     weak var counterLabel: UILabel!
     
+    @IBOutlet
+    weak var historyTextView: UITextView!
+    
     private var counter: Int = 0
+    private var history: String = "История изменений:"
+    
+    private var timeStamp: String {
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        formatter.dateStyle = .short
+        return formatter.string(from: currentDateTime)
+    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        historyTextView.text = history
         clearUI()
     }
     
@@ -28,26 +41,32 @@ class ViewController: UIViewController {
     
     private func updateUI() {
         counterLabel.text = "Значение счётчика: \(counter)"
+        historyTextView.text = history
+        // Scroll to bottom
+        let bottom = NSMakeRange(historyTextView.text.count - 1, 1)
+        historyTextView.scrollRangeToVisible(bottom)
     }
     
     // MARK: - IBActions
     @IBAction func clearButtonDidTapped(_ sender: UIButton) {
+        history.append(contentsOf: "\n [\(timeStamp)]: значение сброшено")
         clearUI()
-        print("Debug: Clear button was tapped")
     }
     
     @IBAction func addButtonDidTapped(_ sender: UIButton) {
         counter += 1
+        history.append(contentsOf: "\n [\(timeStamp)]: значение изменено на +1")
         updateUI()
-        print("Debug: Add button was tapped")
     }
     
     @IBAction func subtractButtonDidTapped(_ sender: UIButton) {
         if counter > 0 {
             counter -= 1
+            history.append(contentsOf: "\n [\(timeStamp)]: значение изменено на -1")
+        } else {
+            history.append(contentsOf: "\n [\(timeStamp)]: попытка уменьшить значение счётчика ниже 0")
         }
         updateUI()
-        print("Debug: Subtract button was tapped")
     }
 }
 
