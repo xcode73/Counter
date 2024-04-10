b3c843c39b23e54b5c3bcf232187285dbd8177d2
xcode73 <nikolai.dev@icloud.com>
2024-04-10 12:01:51 +0400

    feat: Basic task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet
+    weak var counterLabel: UILabel!
+    
+    private var counter: Int = 0
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        clearUI()
     }
-
-
+    
+    private func clearUI() {
+        counter = 0
+        updateUI()
+    }
+    
+    private func updateUI() {
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
+    // MARK: - IBActions
+    @IBAction func clearButtonDidTapped(_ sender: UIButton) {
+        clearUI()
+        print("Debug: Clear button was tapped")
+    }
+    
+    @IBAction func addButtonDidTapped(_ sender: UIButton) {
+        counter += 1
+        updateUI()
+        print("Debug: Add button was tapped")
+    }
 }
 
